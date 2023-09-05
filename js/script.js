7926d63d2298f76ee18109c5ded16cc37cdf0d80
karbelius <132201605+karbelius@users.noreply.github.com>
2023-09-05 09:54:46 +0200

    added new code and pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-var sayMyName = function (name) {
+// comment
+
+/*
+a
+long
+comment
+*/
+
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+	
+}
+
+var sayMyName = function (name)  {
 	alert('My name is: '+name);
 }
 
